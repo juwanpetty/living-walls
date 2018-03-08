cd7478a0f761c9c4175f1b136910a330e0d50aaf
Juwan Petty <jchpetty@gmail.com>
2018-03-08 16:00:07 -0500

    Add new, create, post_params in photos_controller.rb and create new.html.erb

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,24 @@ class PhotosController < ApplicationController
 
     def index
     end
-    
+
+    def new 
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new post_params
+
+        if @post.save
+            redirect_to @post, notice: "Your article was successfuly saved."
+        else
+            render 'new', notice: "Your article was not saved successfully."
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:location)
+    end
 
 end
