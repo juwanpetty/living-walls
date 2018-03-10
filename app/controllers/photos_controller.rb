c92a14d64f885327fc3433a575011d54851ade55
Juwan Petty <jchpetty@gmail.com>
2018-03-10 08:52:44 -0500

    - Add Photo CRUD ability - Update edit and new form with bootstrap for PhotosController - Wrap forms in bootstrap container

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,7 @@
 class PhotosController < ApplicationController
 
+    before_action :find_photo, only: [:show, :edit, :update, :destroy]
+
     def index
     end
 
@@ -11,16 +13,39 @@ class PhotosController < ApplicationController
         @photo = Photo.new photo_params
 
         if @photo.save
-            redirect_to @photo, notice: "Your article was successfuly saved."
+            redirect_to @photo, notice: "Your photo was successfuly saved."
         else
-            render 'new', notice: "Your article was not saved successfully."
+            render 'new', notice: "Your photo was not saved successfully."
         end
+    end
+
+    def show
+    end
+
+    def edit 
+    end
+
+    def update
+         if @photo.update photo_params
+            redirect_to @photo, notice: 'Your photo was successfuly updated.' 
+         else
+            render 'edit', notice: "Your photo was not updated successfully."
+         end
+    end
+
+    def destroy 
+        @photo.destroy
+        redirect_to root_path
     end
 
     private
 
     def photo_params
         params.require(:photo).permit(:location)
+    end
+
+    def find_photo
+        @photo = Photo.find(params[:id])
     end
 
 end
