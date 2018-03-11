efc11d5c7f0dc9f13ec912d06a385388acfd2e0f
Juwan Petty <jchpetty@gmail.com>
2018-03-11 13:34:50 -0400

    Remove unneeded image size: thumbnail from Photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,6 @@ class Photo < ApplicationRecord
     validates :image, presence: true
     validates :location, presence: true
 
-    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :image, styles: { medium: "300x300>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
