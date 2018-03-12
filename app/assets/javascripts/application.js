0ee6d2104d9cf2055c059c4e67a6f11112f5a97b
Juwan Petty <jchpetty@gmail.com>
2018-03-11 22:47:05 -0400

    Configure jquery to the rails applicaiton

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require_tree .
 //= require jquery3
+//= require jquery_ujs
+//= require_tree .
 //= require popper
 //= require bootstrap-sprockets
